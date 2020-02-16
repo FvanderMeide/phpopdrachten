ef0ae0221d6026874c917a34954edabee44bc1b9
Fran van der Meide <fran.vandermeide@edu-kw1c.nl>
2020-02-16 22:19:45 +0100

    Uitwerkingen 2.1

--- Test.php
+++ Test.php
@@ -1,13 +1,20 @@
+<!DOCTYPE html>
+
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <meta name="description" content="opdracht X">
+    <meta name="keywords" content="keyword 1, keyword 2">
+    <link rel="stylesheet" type="text/css" href="les1.css">
+    <title>
+        <Html>
+    </title>
 </head>
 <body>
-<h1>
-    <?php
-    echo "hello wereld";
-    ?>
-</h1>
+    <h1>
+        <?php
+        echo "hello wereld";
+        ?>
+    </h1>
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: Fran van der Meide
+ * Date: Date
+ * Time: **:** PM
+ * File: index.php
+ */
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="opdracht X">
+    <meta name="keywords" content="keyword 1, keyword 2">
+    <link rel="stylesheet" type="text/css" href="">
+    <title>
+        index
+    </title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                 <ul>
+                    <li>
+                        <a href=hoofdstuk2/opdracht_2.1.php>Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+</body>
+</html>
+
